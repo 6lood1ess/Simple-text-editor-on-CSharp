0e092cc8cb16dbe9a520123fc65caa142ee297dc
6lood1ess <6lo.od1ess@gmail.com>
2026-03-18 19:24:50 +0700

    - Add FileSearcher.cs with SearchByKeywords method - Implement directory scanning for .txt files - Add case-insensitive keyword matching - Return list of files containing all keywords

---
+++ FileSearcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class FileSearcher {
+  public List<string> SearchByKeywords(string directory, string[] keywords) {
+
+    var result = new List<string>();
+    var files = Directory.GetFiles(directory, "*.txt");
+
+    foreach (var file in files) {
+
+      var content = File.ReadAllText(file);
+
+      if (keywords.Any(keyword => content.Contains(keyword, StringComparison.OrdinalIgnoreCase))) {
+        result.Add(file);
+      }
+    }
+
+    return result;
+  }
+}
