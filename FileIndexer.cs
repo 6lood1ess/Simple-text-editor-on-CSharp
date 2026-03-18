38a77bab7243fabb2b8f81519bf8ca51095c1cb0
6lood1ess <6lo.od1ess@gmail.com>
2026-03-18 20:04:45 +0700

    - Add FileIndexer.cs with Dictionary<string, List<string>> keywordToFilesMap - Create BuildIndexForDirectory() method to scan files and build index - Implement DisplayIndexResults() to display indexing results - Add case-insensitive keyword matching - Clear index before each new indexing operation

---
+++ FileIndexer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class FileIndexer {
+ 
+  private Dictionary<string, List<string>> _keywordToFilesMap = new Dictionary<string, List<string>>();
+
+  public void BuildIndexForDirectory(string targetDirectory, string[] keywordsToIndex) {
+
+    _keywordToFilesMap.Clear();
+
+    var textFilesInDirectory = Directory.GetFiles(targetDirectory, "*.txt");
+
+    foreach (var keyword in keywordsToIndex) {
+      _keywordToFilesMap[keyword] = new List<string>();
+    }
+
+    foreach (var filePath in textFilesInDirectory) {
+
+      var fileContent = File.ReadAllText(filePath);
+
+      foreach (var keyword in keywordsToIndex) {
+        if (fileContent.Contains(keyword, StringComparison.OrdinalIgnoreCase)) {
+          _keywordToFilesMap[keyword].Add(filePath);
+        }
+      }
+    }
+  }
+
+  public void DisplayIndexResults() {
+
+    foreach (var keywordEntry in _keywordToFilesMap) {
+      Console.WriteLine($"Keyword: {keywordEntry.Key}");
+      foreach (var filePath in keywordEntry.Value) {
+        Console.WriteLine($"  - {filePath}");
+      }
+    }
+  }
+}
