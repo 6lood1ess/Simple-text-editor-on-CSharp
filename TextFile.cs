875a47d6037d7433754088821fe5b608b5c7b2ef
6lood1ess <6lo.od1ess@gmail.com>
2026-03-18 19:07:23 +0700

    - Add TextFile.cs with core properties (FilePath, Content, LastModified) - Implement Load() and Save() methods for file operations - Add BinarySerialize() and BinaryDeserialize() methods - Add XmlSerialize() and XmlDeserialize() using XmlSerializer - Include parameterless constructor for XML serialization"

---
+++ TextFile.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+[Serializable]
+public class TextFile {
+
+  public string FilePath { get; set; }
+  public string Content { get; set; }
+  public DateTime LastModified { get; set; }
+
+  public TextFile() { } //для сериализации
+
+  public TextFile(string path) {
+    FilePath = path;
+    Load();
+  }
+
+  public void Load() => Content = File.ReadAllText(FilePath);
+  public void Save() => File.WriteAllText(FilePath, Content);
+
+  public void BinarySerialize(string path) {
+
+    using (var stream = new FileStream(path, FileMode.Create)) {
+      using (var writer = new BinaryWriter(stream)) {
+        writer.Write(FilePath);
+        writer.Write(Content);
+        writer.Write(LastModified.Ticks);
+      }
+    }
+  }
+
+  public static TextFile BinaryDeserialize(string path) {
+
+    using (var stream = new FileStream(path, FileMode.Open)) {
+      using (var reader = new BinaryReader(stream)) {
+
+        return new TextFile {
+          FilePath = reader.ReadString(),
+          Content = reader.ReadString(),
+          LastModified = new DateTime(reader.ReadInt64())
+        };
+      }
+    }
+  }
+
+  public void XmlSerialize(string path) {
+
+    var xml = new XmlSerializer(typeof(TextFile));
+
+    using (var stream = new FileStream(path, FileMode.Create)) {
+      xml.Serialize(stream, this);
+    }
+  }
+
+  public static TextFile XmlDeserialize(string path) {
+
+    var xml = new XmlSerializer(typeof(TextFile));
+
+    using (var stream = new FileStream(path, FileMode.Open)) {
+      return (TextFile)xml.Deserialize(stream);
+    }
+  }
+}
