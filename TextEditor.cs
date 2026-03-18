582e96ae33f54d4d744b23c17da64633482e5a08
6lood1ess <6lo.od1ess@gmail.com>
2026-03-18 19:43:29 +0700

    - Create TextEditor.cs with private Memento nested class - Implement Open() method to load files - Add Edit() method with automatic snapshot saving - Create Undo() method for reverting changes - Add Save() and GetContent() helper methods - Use Stack for history management

---
+++ TextEditor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TextEditor {
+
+  private TextFile _currentFile;
+  private Stack<Memento> _history = new Stack<Memento>();
+
+  private class Memento {
+    public string Content { get; set; }
+    public DateTime Timestamp { get; set; }
+  }
+
+  public void Open(string path) {
+    _currentFile = new TextFile(path);
+    SaveSnapshot();
+  }
+
+  public void Edit(string newContent) {
+    _currentFile.Content = newContent;
+    SaveSnapshot();
+  }
+
+  private void SaveSnapshot() {
+    _history.Push(new Memento { Content = _currentFile.Content, Timestamp = DateTime.Now });
+  }
+
+  public void Undo() {
+    if (_history.Count > 1) {
+      _history.Pop();
+      _currentFile.Content = _history.Peek().Content;
+    }
+  }
+
+  public void Save() => _currentFile.Save();
+  public string GetContent() => _currentFile.Content;
+}
