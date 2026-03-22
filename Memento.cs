502bc3dc10b765cdc6ab4541dbffcbc0196e1a3a
6lood1ess <6lo.od1ess@gmail.com>
2026-03-22 10:55:19 +0700

    - Added a Memento class

---
+++ Memento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+[Serializable]
+public class Memento {
+
+  public string Content;
+  public DateTime Timestamp;
+
+  public Memento(string content) {
+    Content = content;
+    Timestamp = DateTime.Now;
+  }
+}
+
